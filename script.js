20bc903312c9a5b0faa63451afc39f545464d7cd
Dan Beasley <dannyleebeasley@gmail.com>
2024-02-09 12:52:50 -0500

    Reworked card animations

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-//--// DEFINE VARIABLES //--//
-//Get the Expand Section Buttons and Sections
+//Define Variables
 let expandBttns = document.querySelectorAll(".expand-bttn");
 let expandSects = document.querySelectorAll(".expnd-sect");
-//Get the Cards
 let cards = document.querySelectorAll(".card");
 let closeBttns = document.querySelectorAll(".close-card-info");
-//--// END DEFINE VARIABLES //--//
 
 
-//--// VIDEO TOGGLE //--//
-//Add click listener to each Expand Section Button
+//Click Listneners
 expandBttns.forEach(function (bttn) {
   bttn.addEventListener("click", expandSection);
 });
-//Add click listener to each Expand Section Button
 expandBttns.forEach(function (bttn) {
   bttn.addEventListener("click", activeButton);
 });
 
-//Expand the Section matching the Expand Section Button clicked
+cards.forEach(function (card) {
+  card.addEventListener("click", activateCard);
+});
+
+
+//Functions
 function expandSection(e) {
   let targetSectId = e.target.dataset.targetSectId;
   expandSects.forEach(function (sect) {
@@ -29,42 +29,35 @@ function expandSection(e) {
   });
 }
 
-//Change appearance of buttons based on active or inactive state
 function activeButton(e) {
   expandBttns.forEach(function (bttn) {
     bttn.classList.remove("active");
   });
   e.target.classList.add("active");
 }
-//--// END VIDEO TOGGLE //--//
 
 
-//--// SITE CARD INFO TOGGLE //--//
-//Add click listner to each each Card
-cards.forEach(function (card) {
-  card.addEventListener("click", toggleCardInfo);
-});
+function revealReadMore() {
+  scrollDistance = window.scrollY;
 
-//Add active class to selected Card.
-function toggleCardInfo() {
-  // remove active class from previously selected cards
   cards.forEach(function (card) {
-    card.classList.remove("active");
+    var distanceFromTop = window.pageYOffset + card.getBoundingClientRect().top;
+
+    if (scrollDistance + 1050 > distanceFromTop) {
+      card.classList.add("tab-show");
+    }
   });
-  // add active class
-  this.classList.add("active");
 }
-//--// END SITE CARD INFO TOGGLE //--//
 
-
-//--// CLOSE CARD INFO //--//
-//Add click listener to all close buttons
-closeBttns.forEach(function (closeBttn) {
-  closeBttn.addEventListener("click", closeCardInfo);
-});
-
-//Remove active class from all cards
-function closeCardInfo() {
-  this.parentElement.parentElement.parentElement.classList.remove("active");
+function activateCard () {
+  if (this.classList.contains("active")) {
+    this.classList.remove("active");
+  } else {
+    cards.forEach(function(card) {
+      card.classList.remove("active");
+    })
+    this.classList.add("active");
+  }
 }
-//--// END CLOSE CARD INFO //--//
+
+window.addEventListener("scroll", revealReadMore);
